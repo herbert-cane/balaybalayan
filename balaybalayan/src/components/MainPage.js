9622e977cd86b8b7854e46cc6d8efe53e3a955ff
Brethren Ace D. de la Gente <bddelagente@up.edu.ph>
2024-10-12 01:37:43 +0800

    Main Page Carousel Update
    Added the SwiperJS module to create a smooth carousel animation for the Dorm Section of the homepage.
    
    This shit is hard as fuck. T_T

--- balaybalayan/src/components/MainPage.js
+++ balaybalayan/src/components/MainPage.js
@@ -1,27 +1,101 @@
 import React from 'react';
-import './main.css'; // Import the CSS file
-import './style.css'; // Ace's CSS file for random shit
-import Navbar from './Navbar'; 
+import { useState } from "react";
+import './script.js';
 import banner from './photos/banner.png'; // Import the banner image
+import './main.css'; // Import the CSS file
+import './swiper-bundle.min.css';
+
 
 
 function MainPage() {
   return (
     <><div>
       <img id="banner" src={banner} alt="Banner" /> {/* Added an ID to the banner */}
-    </div><div class="slide-container">
-      <div class ="card-wrapper">
-        <div class="card">
-          <div class="image-content">
-            <span class="overlay"></span>
-
-            <div class="card-image">
-              <img src="photos/dorm1.jpg" alt="" class="card-img"></img>
+    </div><div class="ex-do">
+      <h2> Explore Dormitories</h2></div><div class="container-fluid swiper">
+      <div class="slideContainer">
+        <div class="card-wrapper swiper-wrapper">
+          <div class="card swiper-slide">
+            <div class="image-box">
+              <img src= {require(".//photos/dorm1.png")} alt="" class="card-img"></img>
             </div>
+            <div class="dorm-type">
+              <img src={require(".//photos/logo4.png")} alt=""></img>
+              <div class="dorm-details">
+                <h3 class="dorm-name">No Villa's</h3>
+                <h5 class="dorm-description">Hatdog</h5>
+              </div>
+            </div>
+            <button class="button">Check Dormitory</button>
+          </div><div class="card swiper-slide">
+            <div class="image-box">
+              <img src= {require(".//photos/dorm2.png")} alt="" class="card-img"></img>
+            </div>
+            <div class="dorm-type">
+              <img src={require(".//photos/logo4.png")} alt=""></img>
+              <div class="dorm-details">
+                <h3 class="dorm-name">No Villa's</h3>
+                <h5 class="dorm-description">Hatdog</h5>
+              </div>
+            </div>
+            <button class="button">Check Dormitory</button>
+          </div><div class="card swiper-slide">
+            <div class="image-box">
+              <img src= {require(".//photos/dorm3.png")} alt="" class="card-img"></img>
+            </div>
+            <div class="dorm-type">
+              <img src={require(".//photos/logo4.png")} alt=""></img>
+              <div class="dorm-details">
+                <h3 class="dorm-name">No Villa's</h3>
+                <h5 class="dorm-description">Hatdog</h5>
+              </div>
+            </div>
+            <button class="button">Check Dormitory</button>
+          </div><div class="card swiper-slide">
+            <div class="image-box">
+              <img src= {require(".//photos/dorm1.png")} alt="" class="card-img"></img>
+            </div>
+            <div class="dorm-type">
+              <img src={require(".//photos/logo4.png")} alt=""></img>
+              <div class="dorm-details">
+                <h3 class="dorm-name">No Villa's</h3>
+                <h5 class="dorm-description">Hatdog</h5>
+              </div>
+            </div>
+            <button class="button">Check Dormitory</button>
+          </div><div class="card swiper-slide">
+            <div class="image-box">
+              <img src= {require(".//photos/dorm2.png")} alt="" class="card-img"></img>
+            </div>
+            <div class="dorm-type">
+              <img src={require(".//photos/logo4.png")} alt=""></img>
+              <div class="dorm-details">
+                <h3 class="dorm-name">No Villa's</h3>
+                <h5 class="dorm-description">Hatdog</h5>
+              </div>
+            </div>
+            <button class="button">Check Dormitory</button>
+          </div><div class="card swiper-slide">
+            <div class="image-box">
+              <img src= {require(".//photos/dorm1.png")} alt="" class="card-img"></img>
+            </div>
+            <div class="dorm-type">
+              <img src={require(".//photos/logo4.png")} alt=""></img>
+              <div class="dorm-details">
+                <h3 class="dorm-name">No Villa's</h3>
+                <h5 class="dorm-description">Hatdog</h5>
+              </div>
+            </div>
+            <button class="button">Check Dormitory</button>
           </div>
         </div>
       </div>
-    </div><div className="amenities">
+      <div class="swiper-button-next swiper-navBtn"></div>
+      <div class="swiper-button-prev swiper-navBtn"></div>
+      <div class="swiper-pagination"></div>
+    </div>
+    <script src={require("https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js")}></script>
+    <script src={require("./script.js")}></script><div className="amenities">
       <h2>Amenities</h2>
       <ul>
         <li>Free Wi-Fi</li>
