39baa7e0630ee33d6db7fce38a2a75523428a89d
Brethren Ace D. de la Gente <bddelagente@up.edu.ph>
2025-01-10 23:05:47 +0800

    Fixing stupid bugs.

--- balaybalayan/src/firebase.js
+++ balaybalayan/src/firebase.js
@@ -7,13 +7,21 @@ import { getStorage } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+
+  apiKey: "AIzaSyCcXzRUkO2wgNlUXKq9Kt5kliyMpJERrYM",
+
+  authDomain: "balay-balayan-b6fba.firebaseapp.com",
+
+  projectId: "balay-balayan-b6fba",
+
+  storageBucket: "balay-balayan-b6fba.appspot.com",
+
+  messagingSenderId: "236004704530",
+
+  appId: "1:236004704530:web:aca3fe51b6464c35ac33cb"
+
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
